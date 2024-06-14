c87f985c7a9d98212d1f4d7d269e92272fdb923b
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-06-14 10:40:25 +0200

    adds cards creation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,7 @@
 const URL = "https://striveschool-api.herokuapp.com/api/product/";
 const auth = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjZiZjk5NzdjMjM5YzAwMTUyZjRiM2MiLCJpYXQiOjE3MTgzNTIyNzksImV4cCI6MTcxOTU2MTg3OX0.iqJ9iKifEmv4QneeBjeH2alL6rTwRy3dLqfFcopl1co ";
+const container = document.querySelector(".container");
+const row = document.querySelector(".row");
 
 const fetchProducts = () => {
   fetch(URL, {
@@ -21,7 +23,51 @@ const fetchProducts = () => {
       if (requestedProducts.length === 0) {
         console.log("There are no products");
       } else {
-        /* DOM MANIPULATION */
+        // DOM MANIPULATION
+        requestedProducts.forEach(product => {
+          // creo il div .col
+          const col = document.createElement("div");
+          col.className = "col-md-4";
+
+          // creo il div .card
+          const card = document.createElement("div");
+          card.className = "card mb-4";
+
+          // creo la top image cap
+          const cardImg = document.createElement("img");
+          cardImg.src = product.imageUrl;
+
+          // creo il card body
+          const cardBody = document.createElement("div");
+          cardBody.className = "card-body";
+
+          // creo il card title
+          const cardTitle = document.createElement("h5");
+          cardTitle.innerText = product.name;
+
+          // creo il card subtitle
+          const cardSubTitle = document.createElement("h6");
+          cardSubTitle.innerText = product.brand;
+
+          // creo il card text
+          const cardText = document.createElement("div");
+          cardText.className = "card-text";
+          cardText.innerText = product.description;
+
+          // creo il prezzo
+          const cardPrice = document.createElement("small");
+          cardPrice.innerText = `$${product.price}`;
+
+          // creo il button modifica
+          const modifyBtn = document.createElement("button");
+          modifyBtn.className = "btn btn-primary";
+          modifyBtn.innerText = "Modify";
+
+          cardBody.append(cardTitle, cardSubTitle, cardText, cardPrice, modifyBtn);
+          card.append(cardImg, cardBody);
+          col.appendChild(card);
+          row.appendChild(col);
+        });
       }
     })
     .catch(error => console.log(error));
